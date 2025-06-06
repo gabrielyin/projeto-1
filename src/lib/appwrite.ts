419bf85eb0236747689059387e4043bd8c78e8b5
gabrielyin <2021gabrielyin@gmail.com>
2025-06-06 14:32:28 -0300

    protecting dashboard route

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, Storage, ID } from "appwrite";
+import { Client, Databases, Storage, ID, Account } from "appwrite";
 
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -7,3 +7,12 @@ const client = new Client()
 export const databases = new Databases(client);
 export const storage = new Storage(client);
 export { ID };
+
+export async function getLoggedInUser() {
+  try {
+    const account = new Account(client);
+    return await account.get();
+  } catch {
+    return null;
+  }
+}
